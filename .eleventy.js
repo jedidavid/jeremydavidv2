ac84a05e873a1694a079102147cef1c455d02149
jedidavid <jeremydb.david@gmail.com>
2024-10-28 10:06:48 +0800

    feat: seo

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/img");
   eleventyConfig.addPassthroughCopy("src/assets/js");
   eleventyConfig.addPassthroughCopy("src/assets/files");
+
   eleventyConfig.addPassthroughCopy({ static: "/" });
 
   eleventyConfig.addCollection("featuredWork", function (collectionApi) {
